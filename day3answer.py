e9370ad242f5c67ff56a271533b78cd4eb39fcd5
Walter-Thompson <walter.thompson@arcadis.com>
2023-12-04 18:12:25 +0000

    done

--- day3answer.py
+++ day3answer.py
@@ -60,3 +60,57 @@ answer = sum(valid_nums)
 print("Part 1 answer is:",answer)
 
 #-------------PART 2----------------#
+#test data
+#data =  ["467..114..","...*......","..35..633.","......#...","617*......",".....+.58.","..592.....","......755.","...$.*....",".664.598.."]
+
+number_matches = []
+symbol_matches = []
+symbol_match_rows = []
+number_match_rows = []
+
+gear_ratios = []
+
+#first, let's find the symbols and numbers
+row_counter = 0
+for row in data:
+    #Find symbols
+    for match in re.finditer(r"\*",row):
+        symbol_match_rows.append(row_counter)
+        symbol_matches.append(match)
+    
+    #Find numbers
+    for match in re.finditer(r"\d+",row):
+        number_match_rows.append(row_counter)
+        number_matches.append(match)
+
+    row_counter+=1
+
+#Belt and braces approach... do it one * at a time.
+
+#build trigger zones in 3x3 grid around symbols
+for i in range(len(symbol_matches)):
+    #initialise trigger_zones fresh each time
+    trigger_zones = np.zeros((len(data),len(data[0])),np.int8)
+    potential_ratios = []
+
+    column = symbol_matches[i].span()[0]
+    row = symbol_match_rows[i]
+    #set trigger zones to 1 around symbols ONE AT A TIME, as we are in the for loop
+    trigger_zones[row-1:row+2,column-1:column+2] = 1
+    #iterate through number matches and check if any point of them lies in the trigger zone
+    for i in range(len(number_matches)):
+        row = number_match_rows[i]
+        span_start = number_matches[i].span()[0]
+        span_end = number_matches[i].span()[1]-1
+        number = int(number_matches[i][0])
+        #Do the check
+        if trigger_zones[row,span_start] == 1 or trigger_zones[row,span_end] == 1:
+            potential_ratios.append(number)
+    
+    if len(potential_ratios) == 2:
+        gear_ratios.append(potential_ratios[0]*potential_ratios[1])
+    
+
+#Part 1 Answer
+answer = sum(gear_ratios)
+print("Part 2 answer is:",answer)
